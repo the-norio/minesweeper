8427fe83491c0c1a7e201349f60aaa7fd4d2c2af
tenori-o <lucas.tnrio@gmail.com>
2023-01-27 08:41:59 -0300

    generating cells in the board and adjusting camera alignment to center

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -21,5 +21,24 @@ public class Game : MonoBehaviour
     private void NewGame()
     {
         state = new Cell[width, height];
+
+        GenerateCells();
+
+        Camera.main.transform.position = new Vector3(width / 2f, height / 2f, -10f);
+        board.Draw(state);
+    }
+
+    private void GenerateCells()
+    {
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                Cell cell = new Cell();
+                cell.position = new Vector3Int(x, y, 0);
+                cell.type = Cell.Type.Empty;
+                state[x, y] = cell;
+            }
+        }
     }
 }
