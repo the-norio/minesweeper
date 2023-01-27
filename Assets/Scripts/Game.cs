7e42512a081ca28ff7fa80324b410602a17ff7f1
tenori-o <lucas.tnrio@gmail.com>
2023-01-27 09:54:37 -0300

    flooding empty spaces when one of them is clicked

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -146,10 +146,30 @@ public class Game : MonoBehaviour
 
         if (cell.type == Cell.Type.Invalid || cell.revealed || cell.flagged)
             return;
+
+        if (cell.type == Cell.Type.Empty)
+            Flood(cell);
         
         cell.revealed = true;
         state[cellPosition.x, cellPosition.y] = cell;
         board.Draw(state);
+    }
+
+    private void Flood(Cell cell)
+    {
+        if (cell.revealed) return;
+        if (cell.type == Cell.Type.Mine || cell.type == Cell.Type.Invalid) return;
+
+        cell.revealed = true;
+        state[cell.position.x, cell.position.y] = cell;
+
+        if (cell.type == Cell.Type.Empty)
+        {
+            Flood(GetCell(cell.position.x - 1, cell.position.y));
+            Flood(GetCell(cell.position.x + 1, cell.position.y));
+            Flood(GetCell(cell.position.x, cell.position.y - 1));
+            Flood(GetCell(cell.position.x, cell.position.y + 1));
+        }
     }
 
     private Cell GetCell(int x, int y)
