489b2a71ac193ab5534ec3f896e509eafd90a73c
tenori-o <lucas.tnrio@gmail.com>
2023-01-27 09:23:00 -0300

    fixing tileNumber getter

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -50,13 +50,13 @@ public class Board : MonoBehaviour
         switch (cell.number)
         {
             case 1: return tileNum1;
-            case 2: return tileNum1;
-            case 3: return tileNum1;
-            case 4: return tileNum1;
-            case 5: return tileNum1;
-            case 6: return tileNum1;
-            case 7: return tileNum1;
-            case 8: return tileNum1;
+            case 2: return tileNum2;
+            case 3: return tileNum3;
+            case 4: return tileNum4;
+            case 5: return tileNum5;
+            case 6: return tileNum6;
+            case 7: return tileNum7;
+            case 8: return tileNum8;
             default: return null;
         }
     }
