d98d0380f23693d9f0656489da459029a53fb167
tenori-o <lucas.tnrio@gmail.com>
2023-01-27 10:12:57 -0300

    setting explode mine, gameover and winning condition

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -8,10 +8,12 @@ public class Game : MonoBehaviour
 
     private Board board;
     private Cell[,] state;
+    private bool gameOver;
 
     private void Awake()
     {
         board = GetComponentInChildren<Board>();
+        gameOver = false;
     }
 
     private void Start()
@@ -115,12 +117,18 @@ public class Game : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetMouseButtonDown(1))
+        if (Input.GetKeyDown(KeyCode.R))
+            NewGame();
+            
+        if (!gameOver)
         {
-            Flag();
-        } else if (Input.GetMouseButtonDown(0))
-        {
-            Reveal();
+            if (Input.GetMouseButtonDown(1))
+            {
+                Flag();
+            } else if (Input.GetMouseButtonDown(0))
+            {
+                Reveal();
+            }
         }
     }
 
@@ -147,12 +155,47 @@ public class Game : MonoBehaviour
         if (cell.type == Cell.Type.Invalid || cell.revealed || cell.flagged)
             return;
 
-        if (cell.type == Cell.Type.Empty)
-            Flood(cell);
-        
-        cell.revealed = true;
-        state[cellPosition.x, cellPosition.y] = cell;
+        switch (cell.type)
+        {
+            case Cell.Type.Mine:
+                Explode(cell);
+                break;
+            case Cell.Type.Empty:
+                Flood(cell);
+                CheckWinCondition();
+                break;
+            default:
+                cell.revealed = true;
+                state[cellPosition.x, cellPosition.y] = cell;
+                break;
+        }
+
         board.Draw(state);
+    }
+
+    private void Explode(Cell cell)
+    {
+        gameOver = true;
+        Debug.Log("You Lose!!");
+
+        cell.revealed = true;
+        cell.exploded = true;
+
+        state[cell.position.x, cell.position.y] = cell;
+
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                cell = state[x, y];
+
+                if (cell.type == Cell.Type.Mine)
+                {
+                    cell.revealed = true;
+                    state[x, y] = cell;
+                }
+            }
+        }
     }
 
     private void Flood(Cell cell)
@@ -169,6 +212,38 @@ public class Game : MonoBehaviour
             Flood(GetCell(cell.position.x + 1, cell.position.y));
             Flood(GetCell(cell.position.x, cell.position.y - 1));
             Flood(GetCell(cell.position.x, cell.position.y + 1));
+        }
+    }
+
+    private void CheckWinCondition()
+    {
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                Cell cell = state[x, y];
+                if (cell.type != Cell.Type.Mine && !cell.revealed)
+                {
+                    return;
+                }
+            }
+        }
+
+        Debug.Log("You won!!");
+        gameOver = true;
+
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                Cell cell = state[x, y];
+
+                if (cell.type == Cell.Type.Mine)
+                {
+                    cell.flagged = true;
+                    state[x, y] = cell;
+                }
+            }
         }
     }
 
