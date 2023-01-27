78263c7802d26a392c9d2c8a51761ed868a6f581
tenori-o <lucas.tnrio@gmail.com>
2023-01-27 09:25:25 -0300

    generating number cells and counting mines for them

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -25,6 +25,7 @@ public class Game : MonoBehaviour
 
         GenerateCells();
         GenerateMines();
+        GenerateNumbers();
 
         Camera.main.transform.position = new Vector3(width / 2f, height / 2f, -10f);
         board.Draw(state);
@@ -67,5 +68,51 @@ public class Game : MonoBehaviour
 
             state[x, y].type = Cell.Type.Mine;
         }
+    }
+
+    private void GenerateNumbers()
+    {
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                Cell cell = state[x, y];
+                
+                if (cell.type == Cell.Type.Mine)
+                    continue;
+                
+                cell.number = CountMines(x, y);
+
+                if (cell.number > 0)
+                    cell.type = Cell.Type.Number;
+                
+                state[x, y] = cell;
+            }
+        }
+    }
+
+    private int CountMines(int cellX, int cellY)
+    {
+        int count = 0;
+
+        for (int adjacentX = -1; adjacentX <= 1; adjacentX++)
+        {
+            for (int adjacentY = -1; adjacentY <= 1; adjacentY++)
+            {
+                if (adjacentX == 0 && adjacentY == 0)
+                    continue;
+
+                int x = cellX + adjacentX;
+                int y = cellY + adjacentY;
+
+                if (x < 0 || x >= width || y < 0 || y >= height)
+                    continue;
+
+                if (state[x, y].type == Cell.Type.Mine)
+                    count++;
+            }
+        }
+
+        return count;
     }
 }
